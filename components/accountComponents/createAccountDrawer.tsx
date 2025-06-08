eed42ac28f3fcd7d2c2852c5e7fca84a2d3e141d
Gurleen <gurleenwadhwa.sk@gmail.com>
2025-06-08 00:38:46 -0300

    Made some file name changes and added a new file

--- components/accountComponents/createAccountDrawer.tsx
+++ components/accountComponents/createAccountDrawer.tsx
@@ -29,7 +29,7 @@ import {
   SelectValue,
 } from "@/components/ui/select"
 import useFetch from "@/hooks/useFetch"
-import { createAccount } from "@/actions/account/createAccount"
+import { createAccount } from "@/actions/account/create"
 import { Loader2 } from "lucide-react"
 import { toast } from "sonner"
 
