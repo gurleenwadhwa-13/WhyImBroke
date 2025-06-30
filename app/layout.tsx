260766f0048a58b0921b0c0c79dbc022ed2ea30c
Gurleen <gurleenwadhwa.sk@gmail.com>
2025-06-30 20:11:43 -0300

    Added a env var for clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,7 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
     >
       <html lang="en">
         <body className={`${inter.className}`}>
