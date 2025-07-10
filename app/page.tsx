85cf72d2fa5d11cc5e9f24da554c503f8da5cc11
Gurleen <gurleenwadhwa.sk@gmail.com>
2025-07-10 19:35:30 -0300

    Fixed path errors

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 export const dynamic = "force-dynamic";
 
-import HeroSection from "@/components/Landing-page/new-hero";
-import WaitlistStats from "@/components/Landing-page/waitlist-stats";
-import Features from "@/components/Landing-page/features";
-import DashboardPreview from "@/components/Landing-page/dashboard-preview";
-import WaitlistSection from "@/components/Landing-page/waitlist-section";
-import Footer from "@/components/Footer/footer";
+import HeroSection from "../components/Landing-page/new-hero"
+import WaitlistStats from "../components/Landing-page/waitlist-stats";
+import Features from "../components/Landing-page/features";
+import DashboardPreview from "../components/Landing-page/dashboard-preview";
+import WaitlistSection from "../components/Landing-page/waitlist-section";
+import Footer from "../components/Footer/footer";
 
 export default function Home() {
   return (
