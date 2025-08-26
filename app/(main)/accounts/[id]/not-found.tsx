591ba01861cd4eda7df3ec93e46c14fb88057eb1
Gurleen <gurleenwadhwa.sk@gmail.com>
2025-08-26 04:59:31 -0300

    added a protected route config in not-found pages, previously redirecting page to root route, now redirects to dashboard

--- app/(main)/accounts/[id]/not-found.tsx
+++ app/(main)/accounts/[id]/not-found.tsx
@@ -1,6 +1,7 @@
 import { Button } from '@/components/ui/button'
 import React from 'react'
 import Link from 'next/link'
+import { SignedIn, SignedOut } from '@clerk/nextjs'
 
 const NotFoundPage = () => {
   return (
@@ -8,9 +9,17 @@ const NotFoundPage = () => {
         <h1 className='text-6xl font-bold gradient-title mb-4'>404</h1>
         <h2 className='text-2xl font-semibold mb-10'>Page Not Found</h2>
         <p className='text-gray-600 mb-8'>Oops! The Page you&apos;re looking for doesn&apos;t exist or has been moved. Please visit the Home Page.</p>
-        <Link href="/">
+        <SignedOut>
+          <Link href="/">
             <Button>Return Home</Button>
-        </Link>
+          </Link>
+        </SignedOut>
+
+        <SignedIn>
+          <Link href="/dashboard">
+            <Button>Return to Dashboard</Button>
+          </Link>
+        </SignedIn>
     </div>
   )
 }
