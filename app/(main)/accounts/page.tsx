7502af4fee82ed22da2985edfb62322c82d33a08
Gurleen <gurleenwadhwa.sk@gmail.com>
2025-09-04 15:24:19 -0300

    added the root-accounts page

---
+++ app/(main)/accounts/page.tsx
@@ -0,0 +1,36 @@
+import { FetchAccounts } from "@/actions/account/fetch-account"
+import AccountCard from "@/app/(main)/dashboard/_components/AccountCard"
+import { getUser } from "@/actions/users/getUser"
+import CreateAccountDrawer from "@/components/Account/create-account-drawer"
+import { Card, CardContent } from "@/components/ui/card"
+import { Plus } from "lucide-react"
+
+
+export default async function AccountsPage() {
+    const { success } = await getUser()
+    if (!success) {
+    return <div className="p-4">Error loading user</div>
+    }
+
+    const { data: accounts } = await FetchAccounts()
+    return (
+        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+        {/* Accounts Grid */}
+        <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+          <CreateAccountDrawer>
+            <Card className="flex m-4 px-5 hover:shadow-md transition-shadow cursor-pointer">
+              <CardContent className="flex flex-col items-center justify-center h-full text-muted-foreground">
+                <Plus className="h-10 w-10 mx-auto mb-2" />
+                <p className="text-m font-medium">Add New Account</p>
+              </CardContent>
+            </Card>
+          </CreateAccountDrawer>
+
+          {accounts.length > 0 &&
+            accounts.map((x) => (
+              <AccountCard key={x.id} account={x} />
+            ))}
+        </div>
+      </div>
+    )
+}
