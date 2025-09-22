8803c4344a96a216ceaafbc97e57d6ff7c9aa644
Gurleen <gurleenwadhwa.sk@gmail.com>
2025-09-22 19:49:39 -0300

    added two new component and a new hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 export const dynamic = "force-dynamic";
 
-import HeroSection from "../components/landing-page/new-hero"
+// import HeroSection from "../components/landing-page/new-hero"
 import Features from "../components/landing-page/features";
 import DashboardPreview from "../components/landing-page/dashboard-preview";
 import WaitlistSection from "../components/landing-page/waitlist-section";
 import Footer from "../components/Footer/footer";
 import Pricing from "@/components/landing-page/pricing";
+import HeroSection from "@/components/latest-hero";
+
 
 export default function Home() {
   return (
