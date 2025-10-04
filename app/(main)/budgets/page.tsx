01431866acfb588bff7f5b7086c364d7c377e72b
Gurleen <gurleenwadhwa.sk@gmail.com>
2025-10-04 05:39:24 -0300

    added new pages

---
+++ app/(main)/budgets/page.tsx
@@ -0,0 +1,19 @@
+export default async function BudgetsPage() {
+    return (
+        <div>
+            {/* Heading */}
+            <div className='border-b border-border p-3'>
+                <div className='flex flex-row items-center justify-between'>
+                <div className='w-[50%]'>
+                    <h1 className="text-3xl font-bold text-foreground">Budgets</h1>
+                    <p className="text-muted-foreground mt-1">Create and Stay on Track of Budgeting</p>
+                </div>
+                </div>
+            </div>
+
+            <div className="flex mx-auto items-center p-3">
+                Coming Soon...
+            </div>
+        </div>
+    )
+}
