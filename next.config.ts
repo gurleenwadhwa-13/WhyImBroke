73e1d8b61c4ea31a1683b01524344e15173e6361
Gurleen <gurleenwadhwa.sk@gmail.com>
2025-10-17 02:30:22 -0300

    finally added posthog to the project

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,21 @@ const nextConfig = {
     dirs: ['app', 'components', 'lib'],
     // Ensure ESLint respects .eslintignore
     ignoreDuringBuilds: true
-  }
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/ingest/static/:path*",
+        destination: "https://us-assets.i.posthog.com/static/:path*",
+      },
+      {
+        source: "/ingest/:path*",
+        destination: "https://us.i.posthog.com/:path*",
+      },
+    ];
+  },
+  // This is required to support PostHog trailing slash API requests
+  skipTrailingSlashRedirect: true,
 };
+
 module.exports = nextConfig;
